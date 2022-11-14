94aebe5c49f26b1da8b8a1a587f5ede2a36e795d
Silver Nhan <votrinhnhan1403@gmail.com>
2022-11-14 14:23:16 -0600

    made the lever

---
+++ Project 428.P3.Group 4/Assets/leverAction_SunMoon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverAction_SunMoon : MonoBehaviour
+{
+    public GameObject sunDrop;
+    public GameObject moonDrop;
+
+    void Start() {
+        sunDrop.SetActive(true);
+        moonDrop.SetActive(false);
+    }
+    public void swapToMoon(){
+        print("swapepd to sun");
+        sunDrop.SetActive(false);
+        moonDrop.SetActive(true);
+    }
+    public void swapToSun(){
+        sunDrop.SetActive(true);
+        moonDrop.SetActive(false);
+        print("swapepd to sun");
+    }
+}
