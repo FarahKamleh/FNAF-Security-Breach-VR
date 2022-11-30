e05142a203970446d146f81234bcbcf7182121c6
Silver Nhan <votrinhnhan1403@gmail.com>
2022-11-30 17:25:04 -0600

    Update TriggerMusic.cs

--- Project 428.P3.Group 4/Assets/TriggerMusic.cs
+++ Project 428.P3.Group 4/Assets/TriggerMusic.cs
@@ -13,6 +13,7 @@ public class TriggerMusic : MonoBehaviour
     public Light RWLight;
     public Light MGLight;
     public Light CLight;
+    public Light ceilingLight;
 
     // Start is called before the first frame update
     void Start()
